d055573456d73ebd09febb8cc65c76b38308ff93
moezkan <m.oezkan@gerhard-schubert.de>
2022-04-24 15:24:27 +0200

    Grid finished

--- hw_2/Gridworld.py
+++ hw_2/Gridworld.py
@@ -7,6 +7,7 @@ possible values:
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 class GridWorld:
 
@@ -18,37 +19,83 @@ class GridWorld:
             np.random.randint(0, width)
         )
 
-
     def make(self, width, height):
         self.height = height
         self.width = width
 
-
+        # Create thr world
         self.world = np.zeros((self.height, self.width))
         self.pos = GridWorld.random_pos(self.height, self.width)
-
+                
         neg_prob = np.random.randint(0, 4) / 10
-        neg_count = width * height * neg_prob
+        neg_count = int(width * height * neg_prob) 
 
+        # neg rewards
         for _ in range(neg_count):
             pos_y, pos_x = GridWorld.random_pos(width, height)
             self.world[pos_y, pos_x] = np.random.randint(10) * (-1)
 
+        # blocked states
         for _ in range(np.random.randint(3, 6)):
             pos_y, pos_x = GridWorld.random_pos(width, height)
             self.world[pos_y, pos_x] = np.nan
 
+        # infal state
         rew_pos = GridWorld.random_pos(width, height)
-
         self.world[rew_pos] = 10
+
+        # remember base world for reset
         self.base_world = self.world
+        self.start_pos = self.pos
+
 
     def reset(self):
-        pass
+        self.world = self.base_world
 
     def step(self, action: tuple):
-        pass
+        """
+        Accepts 4 different value combinations
+        (0, 1): move to the right
+        (1, 0): move up
+
+        (0, -1): move to the left
+        (-1, 0): move down
+
+        Returns the respective reward and new state
+        """
+
+        curr_y, curr_x = self.pos
+        new_y, new_x = curr_y + action[0], curr_x + action[1]
+
+        if (self.world[new_y, new_x] == np.nan):
+            return self.pos, 0, False
+
+        # check the boundaries
+        self.pos = (
+            min(max(0, new_y), self.height),
+            min(max(0, new_x), self.width),
+        )
+
+        reward = self.world[self.pos[0], self.pos[1]]
+        return self.pos, reward, reward > 0
+
 
     def visualize(self):
-        pass
         
+        canvas = np.zeros_like(self.world)
+        pos_mask = self.world > 0
+        neg_mask = self.world < 0
+
+        block_mask = self.world == np.nan
+        neutral_mask = self.world == 0
+
+        canvas[pos_mask] = 1
+        canvas[neg_mask] = 2
+        canvas[block_mask] = 3
+        canvas[neutral_mask] = 4
+
+        canvas[self.pos[0], self.pos[1]] = 5
+
+        plt.imshow(canvas), plt.axis('off')
+        plt.legend()
+        plt.show()        
